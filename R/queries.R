b526778ba016e6b96962e4d5824ba12c0160353b
Vince Carey <stvjc@channing.harvard.edu>
2021-03-08 14:09:57 -0500

    now processing with sanchit's fix to rtracklayer

--- R/queries.R
+++ R/queries.R
@@ -1,4 +1,6 @@
 #' very speculative approach to creating a query
+#' @param token1 character(1) defaults to "cell_type"
+#' @param query_val character(1) defaults to "GM12878" could be any supported cell type or line tag
 metadata_query_builder = function(token1="cell_type", query_val="GM12878") {
   t1 = "http://bedbase.org/_private_api/query/bedfiles/other-%3E%3E%27"
   t2 = "%27%3D%25s?query_val="
@@ -21,3 +23,42 @@ get_bb_metadata = function(query_type="cell_type", query_val="GM12878") {
   q = metadata_query_builder(token1=query_type, query_val=query_val)
   httr::GET(q)
 }
+
+redirect_handler <- function(url) {
+#
+# thanks to Sanchit Saini, https://github.com/lawremi/rtracklayer/issues/42#issuecomment-790801420
+#
+    tryCatch(
+        expr = {
+            response <- RCurl::getURL(url, header = T)
+            headers <- strsplit(response, "\r\n")[[1]]
+            position <- grep("location:", headers)
+            redirected_url <- sub("location: ", "", headers[position])
+            if (redirected_url != "NA")
+                redirected_url
+            else url
+            },
+        error = function(e) {
+                return(url)
+            }
+    )
+}
+
+build_bb_query = function(md5sum="78c0e4753d04b238fc07e4ebe5a02984") {
+    init = paste("http://bedbase.org/api/bed/", md5sum, "/file/bigbedfile", sep="")
+    redirect_handler(init)
+}
+#' query a bedbase bigbed file
+#' @importFrom RCurl getURL
+#' @param md5sum character(1) md5sum used as key into bedbase resources
+#' @param \dots passed to rtracklayer::import.bb, which will have format="bigBed" set
+#' @examples
+#' b1 = query_bb(md5sum="78c0e4753d04b238fc07e4ebe5a02984", 
+#'     which=GenomicRanges::GRanges("chr17:37000000-39000000"))
+#' b1
+#' @export
+query_bb = function(md5sum, ...) {
+  q = build_bb_query(md5sum)
+  rtracklayer::import.bb(q, format="bigBed", ...)
+}
+    
