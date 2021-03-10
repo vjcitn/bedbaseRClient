557f83a4f3dc0e02a4720ceb1406b35159ce36ac
Vince Carey <stvjc@channing.harvard.edu>
2021-03-10 06:18:04 -0500

    update pkgdown material and metadata element

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' metadata table for encode components of bedbase
+#' @docType data
+#' @format data.frame
+#' @note A column "cell_type_onto" was added, after searching UBERON, Cell Ontology, Cell Line Ontology
+#' for exact matches (module 'cell' in Cell Line Ontology), using ontoProc images in Bioc 3.12.
+#' @examples
+#' names(meta_2021_03_09)
+#' head(meta_2021_03_09)
+"meta_2021_03_09"
+
